494488a166ee2e4e8690573f4106efc11c59ce97
Rodriguez-GD <gdrodriguez1352@gmail.com>
2025-03-04 21:46:58 -0600

    Primera prueba remota3

---
+++ G6test/src/Personas.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Rodriguez
+ */
+// Superclase Persona (abstracta)
+public abstract class Personas {
+    private String nombre;
+    private int edad;
+
+    // Constructor
+    public Personas(String nombre, int edad) {
+        this.nombre = nombre;
+        this.edad = edad;
+    }
+
+    // Métodos getter y setter
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    // Método abstracto
+    public abstract void mostrarProfesion();
+}
